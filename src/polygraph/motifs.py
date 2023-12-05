a667c08afb3c5a7d780c56b5bdaba34cb5b88201
lala8 <lal.avantika@gene.com>
2023-12-05 23:28:45 +0000

    another pandas compatibility fix; added docstrings to motifs.py

--- src/polygraph/motifs.py
+++ src/polygraph/motifs.py
@@ -13,11 +13,23 @@ from polygraph.stats import groupwise_fishers, groupwise_mann_whitney
 
 def process_motif(motif, pseudocounts=1e-2, p=1e-3):
     """
-    Compute log odds and threshold for a PWM
+    Compute log odds and threshold for a PWM.
+
+    Args:
+        motif (Bio.motif): Biopython motif object
+        pseudocounts (float): Pseudocount to add to matrix before log odds calculation
+        p (float): p-value for motif match threshold
+
+    Returns
+        motif (Bio.motif): Processed motif object, containing attributes `log_odds`
+            and `threshold`.
     """
+    # Calculate log odds (PWM) matrix using pseudocount
     motif.log_odds = list(
         motif.counts.normalize(pseudocounts=pseudocounts).log_odds().values()
     )
+
+    # Calculate p-value threshold using MOODS
     motif.threshold = MOODS.tools.threshold_from_p(
         motif.log_odds, MOODS.tools.flat_bg(4), p
     )
@@ -27,6 +39,13 @@ def process_motif(motif, pseudocounts=1e-2, p=1e-3):
 def moods_scan(seq, motifs):
     """
     Scan a sequence with a list of motifs using MOODS
+
+    Args:
+        seq (str): DNA sequence
+        motifs (list): List of Bio.motif objects
+
+    Returns:
+        MOODS scan results
     """
     return MOODS.scan.scan_dna(
         seq,
@@ -47,21 +66,37 @@ def _motif_count(seq, motifs, normalize=True):
     return [len(r) / div for r in moods_scan(seq, motifs)]
 
 
-def motif_frequencies(df, motifs, normalize=True, num_workers=8):
+def motif_frequencies(
+    seqs, motifs, normalize=True, num_workers=8, pseudocounts=1e-2, p=1e-3
+):
     """
     Count frequency of occurrence of motifs in a list of sequences
+
+    Args:
+        seqs (pd.DataFrame): Pandas dataframe containing DNA sequences.
+        motifs (list): List of Bio.motif objects
+        normalize (bool): Whether to normalize the resulting count matrix
+            to correct for sequence length
+        num_workers (int): Number of parallel workers for scanning
+        pseudocounts (float): Pseudocount to add to matrix before log odds calculation
+        p (float): p-value for motif match threshold
+
+    Returns:
+        cts (pd.DataFrame): Count matrix with rows = sequences and columns = motifs
     """
     print("Processing motifs")
-    motifs = [process_motif(motif) for motif in motifs]
+    motifs = [process_motif(motif, pseudocounts=pseudocounts, p=p) for motif in motifs]
 
     print("Scanning")
     with mp.Pool(num_workers) as pool:
-        items = zip(df.Sequence.tolist(), [motifs] * len(df), [normalize] * len(df))
+        items = zip(
+            seqs.Sequence.tolist(), [motifs] * len(seqs), [normalize] * len(seqs)
+        )
         cts = pool.starmap(_motif_count, items)
 
     print("Assembling count matrix")
     cts = pd.DataFrame(np.vstack(cts))
-    cts.index = df.SeqID.tolist()
+    cts.index = seqs.SeqID.tolist()
     cts.columns = [motif.name for motif in motifs]
     return cts
 
@@ -93,46 +128,78 @@ def _scan_seq(seq, motifs, seq_id=None):
     return results
 
 
-def scan_seqs(df, motifs, p=1e-3, pseudocounts=1e-2, num_workers=8, group_col="Group"):
+def scan_seqs(
+    seqs, motifs, num_workers=8, group_col="Group", pseudocounts=1e-2, p=1e-3
+):
     """
     Scan sequences with motifs using MOODS and return a dataframe
-    """
 
+    Args:
+        seqs (pd.DataFrame): Pandas dataframe containing DNA sequences.
+        motifs (list): List of Bio.motif objects
+        num_workers (int): Number of parallel workers for scanning
+        group_col (str): Name of column in `seqs` that contains group IDs
+        pseudocounts (float): Pseudocount to add to matrix before log odds calculation
+        p (float): p-value for motif match threshold
+
+    Returns:
+        cts (pd.DataFrame): Count matrix with rows = sequences and columns = motifs
+    """
     print("Processing motifs")
-    motifs = [process_motif(motif) for motif in motifs]
+    motifs = [process_motif(motif, pseudocounts=pseudocounts, p=p) for motif in motifs]
 
     print("Scanning")
     with mp.Pool(num_workers) as pool:
-        items = zip(df.Sequence.tolist(), [motifs] * len(df), df.SeqID.tolist())
+        items = zip(seqs.Sequence.tolist(), [motifs] * len(seqs), seqs.SeqID.tolist())
         sites = pool.starmap(_scan_seq, items)
 
+    # Construct dataframe of results
     sites = pd.concat(sites).reset_index(drop=True)
-    sites = sites.merge(df[["SeqID", group_col]], on="SeqID", how="left")
+    sites = sites.merge(seqs[["SeqID", group_col]], on="SeqID", how="left")
     return sites
 
 
 def nmf(counts, seqs, reference_group, group_col="Group", n_components=10):
     """
-    Perform NMF on motif count table
-    """
+    Perform NMF on motif count matrix
 
+    Args:
+        counts (pd.DataFrame): motif count matrix where rows are sequences and columns
+            are motifs.
+        seqs (pd.DataFrame): pandas dataframe containing DNA sequences.
+        reference_group (str): ID for the group to use as reference
+        group_col (str): Name of the column in `seqs` containing group IDs
+        n_components (int): Number of components or factors to extract using NMF
+
+    Returns:
+        W (pd.DataFrame): Pandas dataframe of size sequences x factors, containing
+            the contribution of each factor to each sequence.
+        H (pd.DataFrame): Pandas dataframe of size factors x motifs, containing the
+            contribution of each motif to each factor.
+        res (pd.DataFrame): Pandas dataframe containing the FDR-corrected significance
+            testing results for factor contribution between groups.
+    """
     # Run NMF
     model = NMF(n_components=n_components, init="random", random_state=0)
 
+    # Obtain W and H matrices
+    W = pd.DataFrame(model.fit_transform(counts.values))  # seqs x factors
+    H = pd.DataFrame(model.components_)  # factors x motifs
+
     # Format W and H matrices
-    W = pd.DataFrame(model.fit_transform(counts.values))
-    H = pd.DataFrame(model.components_)
     factors = [f"factor_{i}" for i in range(n_components)]
     W.index = counts.index
     W.columns = factors
     H.index = factors
     H.columns = counts.columns
 
-    # Significance testing
+    # Add group IDs to W
     W[group_col] = seqs[group_col].tolist()
 
+    # Significance testing between groups
     res = pd.DataFrame()
     for col in W.columns[:-1]:
+        # For each factor, test whether its abundance differs between groups
         factor_res = groupwise_mann_whitney(
             W, val_col=col, reference_group=reference_group, group_col=group_col
         )
@@ -155,10 +222,26 @@ def motif_combinations(
 ):
     """
     Count occurences of pairwise combinations of motifs and compare between groups
+
+    Args:
+        counts (pd.DataFrame): Pandas dataframe containing the motif count matrix.
+            Rows should be sequences and columns should be motifs.
+        seqs (pd.DataFrame): Pandas dataframe containing sequences
+        reference_group (str): ID of group to use as reference
+        group_col (str): Name of column in `seqs` containing group IDs
+        min_group_freq (int): Limit to combinations with this number of occurences in
+            at least one group.
+        min_group_prop (float): Limit to combinations with this proportion of occurences
+            in at least one group.
+
+    Returns:
+        res (pd.DataFrame): Pandas dataframe containing FDR-corrected significance
+            testing results for the occurrence of pairwise combinations between groups
     """
 
     print("Listing motif combinations")
-    # Get set of motifs present in each sequence
+
+    # Get the complete set of motifs present in each sequence
     motif_combinations = counts.apply(
         lambda row: set(counts.columns[np.where(row > 0)[0]]), axis=1
     ).reset_index()
@@ -176,8 +259,12 @@ def motif_combinations(
     )
 
     print("Making count matrix")
-    # Count number of sequences in which each motif combination is present
-    cts = motif_combinations[["combination", "Group"]].value_counts().reset_index()
+    # Count the number of sequences in which each motif combination is present
+    cts = (
+        motif_combinations[["combination", "Group"]]
+        .value_counts()
+        .reset_index(name="count")
+    )
 
     print("Filtering")
     # Drop rare combinations
@@ -191,6 +278,7 @@ def motif_combinations(
             motif_combinations.combination.isin(sel_comb)
         ]
         cts = cts[cts.combination.isin(sel_comb)]
+
     elif min_group_prop is not None:
         cts["group_total"] = seqs.Group.value_counts()[cts.Group].tolist()
         cts["group_prop"] = cts["count"] / cts.group_total
@@ -203,9 +291,7 @@ def motif_combinations(
         cts = cts[cts.combination.isin(sel_comb)]
 
     print("Significance testing")
-    # Significance testing
     df = seqs[["SeqID", "Group"]].copy()
-
     res = pd.DataFrame()
 
     for comb in cts.combination:
